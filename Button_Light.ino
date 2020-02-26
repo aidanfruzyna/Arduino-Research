32bc96350257232fbb0d4adf5c8a3e241247249a
aidanfruzyna <61519127+aidanfruzyna@users.noreply.github.com>
2020-02-26 14:16:14 -0600

    Button Light Code
    Code set up to light an LED determined by if a button is pressed.

---
+++ Button_Light.ino
@@ -0,0 +1,27 @@
+const int led1=11;
+const int led2=10;
+const int button1=13;
+const int button2=12;
+
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+}
+
+void loop() {
+  int button1State=digitalRead(button1);
+  int button2State=digitalRead(button2);
+
+  if(button1State==LOW) {
+    digitalWrite(led1, HIGH);
+  } else{
+    digitalWrite(led1,LOW);
+}
+if(button2State==LOW) {
+    digitalWrite(led2, HIGH);
+  } else{
+    digitalWrite(led2,LOW);
+  }
+}
